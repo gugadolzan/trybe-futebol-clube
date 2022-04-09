e287ece0831db91e46b9280d6036f1ac88826443
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-09 15:20:54 -0300

    feat(sequelize): create club model

---
+++ app/backend/src/database/models/club.model.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Match from './match.model';
+
+class Club extends Model {
+  public id!: number;
+
+  public clubName!: string;
+}
+
+Club.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    clubName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  },
+  {
+    sequelize: db,
+    tableName: 'clubs',
+    // Removes createdAt and updatedAt timestamps to the model
+    timestamps: false,
+    // Converts all camelCased columns to snake_case
+    underscored: true,
+  },
+);
+
+// Associations
+Match.belongsTo(Club, { foreignKey: 'homeTeam', as: 'homeTeam' });
+Match.belongsTo(Club, { foreignKey: 'awayTeam', as: 'awayTeam' });
+
+Club.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeTeamMatches' });
+Club.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayTeamMatches' });
+
+export default Club;
