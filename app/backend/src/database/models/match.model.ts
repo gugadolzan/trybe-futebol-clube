f10b95f964855885c02c567aa076532c1ca587f0
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-09 15:21:06 -0300

    feat(sequelize): create match model

---
+++ app/backend/src/database/models/match.model.ts
@@ -0,0 +1,69 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  public id!: number;
+
+  public homeTeam!: number;
+
+  public homeTeamGoals!: number;
+
+  public awayTeam!: number;
+
+  public awayTeamGoals!: number;
+
+  public inProgress!: boolean;
+}
+
+Match.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    homeTeam: {
+      allowNull: false,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      references: {
+        model: 'clubs',
+        key: 'id',
+      },
+      type: DataTypes.INTEGER,
+    },
+    homeTeamGoals: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    awayTeam: {
+      allowNull: false,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      references: {
+        model: 'clubs',
+        key: 'id',
+      },
+      type: DataTypes.INTEGER,
+    },
+    awayTeamGoals: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    inProgress: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize: db,
+    tableName: 'matchs',
+    // Removes createdAt and updatedAt timestamps to the model
+    timestamps: false,
+    // Converts all camelCased columns to snake_case
+    underscored: true,
+  },
+);
+
+export default Match;
