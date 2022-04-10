01139b2428a7dd4fc7dc4d2b2e1072ef124c1fd6
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-10 20:01:28 -0300

    feat(clubs): implement GET /clubs/:id endpoint

--- app/backend/src/routes/club.route.ts
+++ app/backend/src/routes/club.route.ts
@@ -4,5 +4,6 @@ import * as controllers from '../controllers';
 const router = Router();
 
 router.get('/', controllers.Club.getAll);
+router.get('/:id', controllers.Club.getById);
 
 export default router;
