1f873f960374962a165e411ae65829a7418da7d1
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-10 18:03:22 -0300

    feat(login): create UserController
    Method login and validateLogin.

---
+++ app/backend/src/controllers/user.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+import * as services from '../services';
+import Jwt from '../utils/Jwt';
+
+export default class UserController {
+  public static async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+    if (!email || !password) throw new Error('All fields must be filled');
+
+    const user = await services.User.login(email, password);
+    if (!user) throw new Error('Incorrect email or password');
+
+    const token = Jwt.generateToken(user);
+    res.status(200).json({ user, token });
+  }
+
+  public static async validateLogin(req: Request & JwtPayload, res: Response) {
+    const { role } = req.user;
+    res.status(200).send(role);
+  }
+}
