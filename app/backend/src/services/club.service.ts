435e2c48ae1fe6a12de185bb2d579b3c8ec1aa39
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-10 20:00:16 -0300

    feat(clubs): implement getById method in ClubService

--- app/backend/src/services/club.service.ts
+++ app/backend/src/services/club.service.ts
@@ -4,4 +4,8 @@ export default class ClubService {
   public static async getAll() {
     return Club.findAll();
   }
+
+  public static async getById(id: number) {
+    return Club.findByPk(id);
+  }
 }
