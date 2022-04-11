c5f985eb741a965d12030aed815d679b82437aea
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-11 13:28:28 -0300

    feat(matches): create methods in controller
    Methods create and finish.

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -1,9 +1,37 @@
 import { Request, Response } from 'express';
+import Match from '../database/models/match.model';
 import * as services from '../services';
 
 export default class MatchController {
   public static async getAll(req: Request, res: Response) {
     const matches = await services.Match.getAll(req.query.inProgress as string);
     res.status(200).json(matches);
+  }
+
+  public static async create(req: Request, res: Response) {
+    const match = {
+      homeTeam: req.body.homeTeam,
+      awayTeam: req.body.awayTeam,
+      homeTeamGoals: req.body.homeTeamGoals || req.body.homeGoals,
+      awayTeamGoals: req.body.awayTeamGoals || req.body.awayGoals,
+      inProgress: true,
+    } as Match;
+
+    if (
+      match.homeTeam === undefined
+      || match.awayTeam === undefined
+      || match.homeTeamGoals === undefined
+      || match.awayTeamGoals === undefined
+    ) {
+      throw new Error('All fields must be filled');
+    }
+
+    const createdMatch = await services.Match.create(match);
+    res.status(201).json(createdMatch);
+  }
+
+  public static async finish(req: Request, res: Response) {
+    const match = await services.Match.finish(Number(req.params.id));
+    res.status(200).json(match);
   }
 }
