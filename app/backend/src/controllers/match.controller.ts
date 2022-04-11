aa61ec0d64b6e27cfbbd36051c2d0acffce574fb
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-10 22:15:43 -0300

    feat(matchs): implement filtering by progress
    Filter matches by progress.
    Implement query string in GET /matchs endpoint.

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import * as services from '../services';
 
 export default class MatchController {
-  public static async getAll(_req: Request, res: Response) {
-    const matches = await services.Match.getAll();
+  public static async getAll(req: Request, res: Response) {
+    const matches = await services.Match.getAll(req.query.inProgress as string);
     res.status(200).json(matches);
   }
 }
--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,9 +1,18 @@
+import * as Sequelize from 'sequelize';
 import Match from '../database/models/match.model';
 import Club from '../database/models/club.model';
 
 export default class MatchService {
-  public static async getAll() {
+  public static async getAll(byProgress?: string) {
+    // If byProgress is defined,
+    // return all matches with the given progress
+    // else return all matches
+    const inProgress = byProgress
+      ? byProgress === 'true'
+      : { [Sequelize.Op.or]: [true, false] };
+
     return Match.findAll({
+      where: { inProgress },
       include: [
         { model: Club, as: 'homeClub', attributes: ['clubName'] },
         { model: Club, as: 'awayClub', attributes: ['clubName'] },
