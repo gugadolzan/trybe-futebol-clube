c587fce23afdbb5080d43495be1d97e8dd7909a5
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-11 13:26:55 -0300

    feat(matchs): create methods in service
    Methods create and finish.

--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,6 +1,7 @@
 import * as Sequelize from 'sequelize';
 import Match from '../database/models/match.model';
 import Club from '../database/models/club.model';
+import ClubService from './club.service';
 
 export default class MatchService {
   public static async getAll(byProgress?: string) {
@@ -18,5 +19,30 @@ export default class MatchService {
         { model: Club, as: 'awayClub', attributes: ['clubName'] },
       ],
     });
+  }
+
+  public static async create(match: Match) {
+    if (match.homeTeam === match.awayTeam) {
+      throw new Error(
+        'It is not possible to create a match with two equal teams',
+      );
+    }
+
+    const [homeClub, awayClub] = await Promise.all([
+      ClubService.getById(match.homeTeam),
+      ClubService.getById(match.awayTeam),
+    ]);
+    if (!homeClub || !awayClub) {
+      throw new Error('There is no team with such id!');
+    }
+
+    return Match.create(match);
+  }
+
+  public static async finish(id: number) {
+    const match = await Match.findOne({ where: { id } });
+    if (!match) throw new Error('There is no match with such id!');
+
+    return match.update({ inProgress: false });
   }
 }
