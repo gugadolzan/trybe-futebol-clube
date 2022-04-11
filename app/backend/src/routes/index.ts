e2fd5d4a15eab423c4e7a6111d698533ac592623
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-10 21:47:37 -0300

    feat(matchs): implement matchs routes
    Endpoint GET /matchs.

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import clubRouter from './club.route';
 import loginRouter from './login.route';
+import matchRouter from './match.route';
 import * as middlewares from '../middlewares';
 
 const routes = Router();
 
 routes.use('/login', loginRouter);
 routes.use('/clubs', clubRouter);
+routes.use('/matchs', matchRouter);
 
 routes.use(middlewares.Error.handleErrors);
 
