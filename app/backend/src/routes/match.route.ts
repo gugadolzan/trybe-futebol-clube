1f95336c0bf18c9214d9a625e397a006ee6cc51a
Gustavo Dolzan <gudolzan@gmail.com>
2022-04-11 13:29:05 -0300

    feat(matchs): configure routes
    Endpoints:
    POST /matchs
    PATCH  /matchs/:id/finish

--- app/backend/src/routes/match.route.ts
+++ app/backend/src/routes/match.route.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import * as controllers from '../controllers';
+import * as middlewares from '../middlewares';
 
 const router = Router();
 
 router.get('/', controllers.Match.getAll);
+router.use(middlewares.Auth.verifyToken);
+router.post('/', controllers.Match.create);
+router.patch('/:id/finish', controllers.Match.finish);
 
 export default router;
